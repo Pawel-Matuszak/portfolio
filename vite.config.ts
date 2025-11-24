4fad256aa71ec1d9227e585422ff8702e8d327f9
pawelmatuszak <pawelxmatuszak@gmail.com>
2025-11-24 16:57:56 +0100

    fix: update base URL for assets and correct icon link paths in HTML files

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/interactive-3d-portfolio/',
   plugins: [react()],
   build: { outDir: 'docs' },
 })
